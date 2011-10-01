8044b95e6acbaf5b97f97345cc50e9bd088b1a58
Stephan Karpischek <kpi@kpi.at>
2011-10-01 14:24:33 +0200

    German translation

--- js/dict.js
+++ js/dict.js
@@ -2,19 +2,19 @@
 
 var translations = {
     de: {
-        'Where Did My Taxes Go?': 'Where Did My Taxes Go?',
-        'Civil Status:': 'Civil Status',
-        'Single': 'Single',
-        'Married': 'Married',
-        'Income 2010': 'Income 2010',
-        'Assets': 'Assets',
-        'Annual Taxes Paid': 'Annual Taxes Paid',
-        'This Money Lasted The City For': 'This Money Lasted The City For',
-        'sec': 'sec',
-        'What is this?': 'What is this?',
+        'Where Did My Taxes Go?': 'Was passiert mit meinen Steuern?',
+        'Civil Status': 'Steuertarif',
+        'Single': 'Grundtarif',
+        'Married': 'Verheiratet',
+        'Income 2010': 'Einkommen 2010',
+        'Assets': 'Vermögen',
+        'Annual Taxes Paid': 'Steuerlast 2010',
+        'This Money Lasted The City For': 'Das finanziert die Stadt für',
+        'sec': 'Sekunden',
+        'What is this?': 'Was ist das?',
         'This was done during the <a href="http://make.opendata.ch/">MakeOpenData</a> camp 2011 using <a href="http://mbostock.github.com/d3/">D3.js</a> and the brainpower of <a href="http://local.ch">Tristan</a>, <a href="http://en.wikipedia.org/wiki/Bernd_Fix">Bernd</a>, <a href="http://kpi.at/">Stephan</a>, <a href="http://seld.be">Jordi</a> and Daniel.':
-            'This was done during the <a href="http://make.opendata.ch/">MakeOpenData</a> camp 2011 using <a href="http://mbostock.github.com/d3/">D3.js</a> and the brainpower of <a href="http://local.ch">Tristan</a>, <a href="http://en.wikipedia.org/wiki/Bernd_Fix">Bernd</a>, <a href="http://kpi.at/">Stephan</a>, <a href="http://seld.be">Jordi</a> and Daniel.',
-        'Data Sources:': 'Data Sources:',
+            'Erstellt im Rahmen von <a href="http://make.opendata.ch/">MakeOpenData</a> Camp 2011. Team: <a href="http://local.ch">Tristan</a>, <a href="http://en.wikipedia.org/wiki/Bernd_Fix">Bernd</a>, <a href="http://www.im.ethz.ch/people/karpischek">Stephan</a>, <a href="http://seld.be">Jordi</a> und Daniel.',
+        'Data Sources:': 'Datenquellen:',
         'Source Code:': 'Source Code:'
     }
 };
